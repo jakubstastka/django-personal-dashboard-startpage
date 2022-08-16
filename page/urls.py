5515f2df01aa1f1ae8d8ad64fb514628faf23aae
Jakub Stastka <jakub@stastka.xyz>
2022-08-16 21:18:15 +0200

    added board adding functionality. more to follow

--- page/urls.py
+++ page/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BoardList
+from .views import BoardList, BoardCreateView
 
 urlpatterns = [
     path('', BoardList.as_view(), name='home'),
+    path('board/create', BoardCreateView.as_view(), name='add-board')
 ]
--- page/views.py
+++ page/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from .models import Board
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 # Create your views here.
 
@@ -12,3 +14,19 @@ class BoardList(LoginRequiredMixin, ListView):
         queryset = super().get_queryset()
         queryset = queryset.filter(user=self.request.user)
         return queryset
+
+
+class BoardCreateView(CreateView):
+    model = Board
+    fields = ['name']
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        print(self.request.user)
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class BoardUpdateView(LoginRequiredMixin, UpdateView):
+    model = Board
+    fields = ['name']
