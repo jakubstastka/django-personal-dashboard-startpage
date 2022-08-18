6718adefa155e3fca5a1570bf484c97d584be219
Jakub Stastka <jakub@stastka.xyz>
2022-08-18 17:59:48 +0200

    added positioning for future implementation

--- page/models.py
+++ page/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Positioned(models.Model):
+    position = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        abstract = True
+        ordering = ('position',)
+
+
 class Timestamped(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -23,7 +31,7 @@ class Named(models.Model):
         return self.name
 
 
-class Board(Timestamped, Named):
+class Board(Positioned, Timestamped, Named):
     description = models.TextField(default="")
     user = models.ForeignKey(User, related_name="boards", on_delete=models.CASCADE)
 
@@ -32,7 +40,7 @@ class Board(Timestamped, Named):
         return self.bookmark_groups.count()
 
 
-class BookmarkGroup(Timestamped, Named):
+class BookmarkGroup(Positioned, Timestamped, Named):
     board = models.ForeignKey(Board, related_name="bookmark_groups", on_delete=models.CASCADE)
 
     @property
@@ -40,7 +48,7 @@ class BookmarkGroup(Timestamped, Named):
         return self.bookmarks.count()
 
 
-class Bookmark(Timestamped, Named):
+class Bookmark(Positioned, Timestamped, Named):
     url = models.URLField(default="")
     bookmark_group = models.ForeignKey(BookmarkGroup, related_name="bookmarks", on_delete=models.CASCADE)
 
