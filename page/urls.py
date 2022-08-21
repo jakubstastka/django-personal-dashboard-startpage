152be557eb0440b6f899ff31ac997900c0b98897
Jakub Stastka <jakub@stastka.xyz>
2022-08-21 20:06:11 +0200

    editing of groups and bookmarks now available

--- page/urls.py
+++ page/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BoardList, BoardCreateView, BoardDeleteView, BoardUpdateView, BookmarkGroupCreateView, BookmarkCreateView, BookmarkGroupDeleteView, BookmarkDeleteView
+from .views import BoardList, BoardCreateView, BoardDeleteView, BoardUpdateView, BookmarkGroupCreateView, BookmarkCreateView, BookmarkGroupDeleteView, BookmarkDeleteView, BookmarkGroupUpdateView
 
 urlpatterns = [
     path('', BoardList.as_view(), name='home'),
@@ -7,7 +7,9 @@ urlpatterns = [
     path('board/<int:pk>/update/', BoardUpdateView.as_view(), name='update-board'),
     path('board/<int:pk>/delete/', BoardDeleteView.as_view(), name='delete-board'),
     path('bookmarkgroup/<int:pk>/create/', BookmarkGroupCreateView.as_view(), name='add-bookmark-group'),
+    path('bookmarkgroup/<int:pk>/update/', BookmarkGroupUpdateView.as_view(), name='update-bookmark-group'),
     path('bookmarkgroup/<int:pk>/delete/', BookmarkGroupDeleteView.as_view(), name='delete-bookmark-group'),
     path('bookmark/<int:pk>/create/', BookmarkCreateView.as_view(), name='add-bookmark'),
+    path('bookmark/<int:pk>/update/', BookmarkCreateView.as_view(), name='update-bookmark'),
     path('bookmark/<int:pk>/delete/', BookmarkDeleteView.as_view(), name='delete-bookmark'),
 ]
--- page/views.py
+++ page/views.py
@@ -47,6 +47,12 @@ class BookmarkGroupCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class BookmarkGroupUpdateView(LoginRequiredMixin, UpdateView):
+    model = BookmarkGroup
+    fields = ['name']
+    success_url = reverse_lazy('home')
+
+
 class BookmarkGroupDeleteView(LoginRequiredMixin, DeleteView):
     model = BookmarkGroup
     success_url = reverse_lazy('home')
@@ -60,6 +66,12 @@ class BookmarkCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.bookmark_group = BookmarkGroup.objects.get(pk=self.kwargs["pk"])
         return super().form_valid(form)
+
+
+class BookmarkUpdateView(LoginRequiredMixin, UpdateView):
+    model = Bookmark
+    fields = ['name', 'url']
+    success_url = reverse_lazy('home')
 
 
 class BookmarkDeleteView(LoginRequiredMixin, DeleteView):
