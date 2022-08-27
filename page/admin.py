fe813feeab869a9cc7f0835cda15928dc00eede1
Jakub Stastka <jakub@stastka.xyz>
2022-08-27 20:26:51 +0200

    using admin.register decorators instead of register methods

--- page/admin.py
+++ page/admin.py
@@ -4,18 +4,16 @@ from page.models import Board, BookmarkGroup, Bookmark
 # Register your models here.
 
 
+@admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
     list_display = ["name", "bookmark_group_count"]
 
 
+@admin.register(BookmarkGroup)
 class BookmarkGroupAdmin(admin.ModelAdmin):
     list_display = ["name", "bookmark_count"]
 
 
+@admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ["name", "url"]
-
-
-admin.site.register(Board, BoardAdmin)
-admin.site.register(BookmarkGroup, BookmarkGroupAdmin)
-admin.site.register(Bookmark, BookmarkAdmin)
