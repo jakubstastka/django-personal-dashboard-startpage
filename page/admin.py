06ad7df886a28a475e7002343ffdacfb32c1bda4
Jakub Stastka <jakub@stastka.xyz>
2022-08-31 01:44:13 +0200

    accountsettings should be registered in admin

--- page/admin.py
+++ page/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from page.models import Board, BookmarkGroup, Bookmark
+from page.models import Board, BookmarkGroup, Bookmark, AccountSettings
 
 # Register your models here.
+
+
+@admin.register(AccountSettings)
+class AccountSettingsAdmin(admin.ModelAdmin):
+    list_display = ["user", "object_count"]
 
 
 @admin.register(Board)
