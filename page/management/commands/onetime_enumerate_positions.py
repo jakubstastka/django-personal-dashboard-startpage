30956332d2ca91ca5d1f0e4ba1e2e4b8a8636856
Jakub Stastka <jakub@stastka.xyz>
2022-08-30 19:22:20 +0200

    mngmnt script to enumerate everything

--- page/management/commands/onetime_enumerate_positions.py
+++ page/management/commands/onetime_enumerate_positions.py
@@ -7,5 +7,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for user in User.objects.all():
             for index, board in enumerate(user.boards.all(), start=1):
+
+                for gindex, group in enumerate(board.bookmark_groups.all(), start=1):
+
+                    for bindex, bookmark in enumerate(group.bookmarks.all(), start=1):
+                        bookmark.position = bindex
+                        bookmark.save()
+
+                    group.position = gindex
+                    group.save()
+
                 board.position = index
                 board.save()
